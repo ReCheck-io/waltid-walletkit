127990963de9ef4987dab68f65498131891cf44a
lestad <j.v.gospodinov@gmail.com>
2022-09-28 11:21:49 +0300

    updt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.31"
+    kotlin("jvm") version "1.7.10"
     kotlin("plugin.serialization") version "1.6.10"
     application
     `maven-publish`
@@ -28,7 +28,7 @@ dependencies {
     implementation("com.github.ajalt.clikt:clikt-jvm:3.4.2")
     implementation("com.github.ajalt.clikt:clikt:3.4.2")
     // SSIKIT
-    implementation("id.walt:waltid-ssi-kit:1.11.0-SNAPSHOT")
+    implementation("id.walt:waltid-ssi-kit:1.12.0")
     implementation("id.walt:waltid-ssikit-vclib:1.22.0")
 
     // Service-Matrix
